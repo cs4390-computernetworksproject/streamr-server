c4b027b1a196110e38825511cee35066a77662be
Jonathan Darling <jmdarling@me.com>
2014-11-13 00:30:39 -0600

    Adds some extra warning error detection.

--- helpers/db_helper.php
+++ helpers/db_helper.php
@@ -6,6 +6,6 @@
 function db_helper_setup() {
   require_once(dirname(__FILE__) . '/../server_config.php');
   $sql = mysqli_connect($_DB_HOST_, $_DB_USER_, $_DB_PASS_) or die("A MySQL error has occurred.<br />Error: (" . mysqli_errno($sql) . ") " . mysqli_error($sql));
-  mysqli_select_db($sql, $_DB_NAME_);
+  mysqli_select_db($sql, $_DB_NAME_) or die("A MySQL error has occurred.<br />Error: (" . mysqli_errno($sql) . ") " . mysqli_error($sql));
   return $sql;
 }
